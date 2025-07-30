40361a1a7e542e1a58798931a2cf5e024b49f7af
VirtuallyExisting <githubbuilder23@outlook.com>
2025-07-30 15:47:22 -0400

    Reduce clutter (By a lot) and fix double-screen bug.

--- src/main.lua
+++ src/main.lua
@@ -1,44 +1,18 @@
 -- NOTICE
--- This is a very basic build of the game that doesn't even contain sprites, 
+-- This is a prototype, nothing is final.
 
 
 
 function love.load()
     local joysticks = love.joystick.getJoysticks()
     joystick = joysticks[1]
-
--- This detector should probably be removed at some point, I have to find another way to get the game working on PC and 3DS, if I can't do that then emulators will have to do for some people.
-    if love._potion_version == nil then
-        platform = computer
-    end
-    if love._potion_version ~= nil then
-        platform = console
-    end
     player = {}
     player.x = 400
     player.y = 200
---    player.xv = 0
---   player.yv = 0
--- These values were removed because it was not the intended movement style for the game.
     player.speed = 3
 end
 
 function love.update(dt)
---    player.x = player.x + player.xv
---    player.y = player.y + player.yv
- --   if player.xv > 0 then
- --       player.xv = player.xv - 0.06
- --   end
-  --  if player.xv < 0 then
-  --      player.xv = player.xv + 0.06
-   -- end
-   -- if player.yv > 0 then
-   --     player.yv = player.yv - 0.06
-   -- end
-  --  if player.yv < 0 then
-   --     player.yv = player.yv + 0.06
-  --  end
--- Don't criticize me on the weirdly positioned comments, they are not my proudest work.
 
 
     if joystick:isGamepadDown("dpleft") then
@@ -55,32 +29,11 @@ function love.update(dt)
     end
 
 
-
-    -- This code has been commented out because the game literally does not function on LOVEPOTION with this existing.
---[[    if platform == computer then
-
-        -- Begin basic movement
-        if love.keyboard.isDown("right") then
-            player.xv = player.xv + 0.12
-        end
-
-        if love.keyboard.isDown("left") then
-            player.xv = player.xv - 0.12
-        end
-        if love.keyboard.isDown("down") then
-            player.yv = player.yv + 0.12
-        end
-
-        if love.keyboard.isDown("up") then
-            player.yv = player.yv - 0.12
-        end
-
-    end]]
 end
 
-
-function love.draw()
+-- Make sure to add screen paramater to love.draw otherwise it renders on both screens.
+function love.draw(screen)
     if screen ~= "bottom" then
         love.graphics.circle("fill", player.x, player.y, 30)
-end
+    end
 end
