8a469a5e41764a01d6196d474fe19202265e8854
VirtuallyExisting <githubbuilder23@outlook.com>
2025-07-30 15:44:46 -0400

    Reduced clutter and changed comments

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,5 @@
 -- NOTICE
--- This is a very basic build of the game that doesn't even contain sprites, 
+-- This is a prototype, nothing is final.
 
 
 
@@ -7,38 +7,13 @@ function love.load()
     local joysticks = love.joystick.getJoysticks()
     joystick = joysticks[1]
 
--- This detector should probably be removed at some point, I have to find another way to get the game working on PC and 3DS, if I can't do that then emulators will have to do for some people.
-    if love._potion_version == nil then
-        platform = computer
-    end
-    if love._potion_version ~= nil then
-        platform = console
-    end
     player = {}
     player.x = 400
     player.y = 200
---    player.xv = 0
---   player.yv = 0
--- These values were removed because it was not the intended movement style for the game.
     player.speed = 3
 end
 
 function love.update(dt)
---    player.x = player.x + player.xv
---    player.y = player.y + player.yv
- --   if player.xv > 0 then
- --       player.xv = player.xv - 0.06
- --   end
-  --  if player.xv < 0 then
-  --      player.xv = player.xv + 0.06
-   -- end
-   -- if player.yv > 0 then
-   --     player.yv = player.yv - 0.06
-   -- end
-  --  if player.yv < 0 then
-   --     player.yv = player.yv + 0.06
-  --  end
--- Don't criticize me on the weirdly positioned comments, they are not my proudest work.
 
     if joystick ~= nil then
         if joystick:isGamepadDown("dpleft") then
@@ -75,9 +50,10 @@ function love.update(dt)
     end
 end
 
-
+-- Add screen into the parameters of love.draw for 3DS branch.
+-- Code still works fine on computer this way, also saves time.
 function love.draw()
     if screen ~= "bottom" then
         love.graphics.circle("fill", player.x, player.y, 30)
-end
+    end
 end
